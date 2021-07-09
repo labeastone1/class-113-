eb205d7d4d8526ed8bb386f6cd599dbbd8d88b00
labeastone1 <87197782+labeastone1@users.noreply.github.com>
2021-07-09 21:22:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function setup(){
+    canvas=createCanvas(640,480);
+    canvas.position(150,150);
+    video=createCapture(VIDEO);
+    video.hide();
+
+    tint_color="";
+
+}
+
+function draw(){
+    image(video,230,150,220,200);
+
+    fill(255,0,0);
+    stroke(255,0,0);
+    circle(50,50,80);
+    circle(590,50,80);
+    circle(590,430,80);
+    circle(50,430,80);
+
+
+    fill(0,120,0);
+    stroke(0,128,0);
+    rect(90,40,460,20);
+    rect(90,420,460,20);
+    rect(90,50,20,390);
+    rect(530,50,20,390)
+}
+
+function take_snapshot(){
+    save('picture_taker.png');
+
+}
